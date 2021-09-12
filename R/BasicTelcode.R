c3313b0a84fd8bd1b4a03bef604e6e542446f9ff
huhuaping <huhuaping01@nwsuaf.edu.cn>
2021-09-12 08:24:11 +0800

    dataset basic telcode

---
+++ R/BasicTelcode.R
@@ -0,0 +1,19 @@
+#' Data Set of Telephone area code information of China
+#'
+#' A data set containing Telephone area code information of china from the github
+#'   repository 'wecatch/china_regions'\url{https://github.com/eduosi/district},
+#'   with wide data format.
+#'
+#' @format A data frame:
+#' \describe{
+#'   \item{name}{ name of city and county }
+#'   \item{code}{ code of city and county }
+#'   \item{area_code}{ code of telephone }
+#'   \item{code_province}{ code of province }
+#'   \item{district}{ name of province }
+#' }
+#'
+#' @source \url{https://www.huhuaping.com/}
+#'
+
+"BasicTelcode"
--- data-raw/district.R
+++ data-raw/district.R
@@ -1,6 +1,7 @@
 ## code to prepare `district` dataset goes here
-library("tidyverse")
 
+##====full district info ====
+library("tidyverse")
 full_name <- c("id", "name", "parent_id", "initial", "initials",
                "pinyin", "extra", "suffix", "code", "area_code", "order")
 
@@ -23,3 +24,36 @@ use_r("BasicDistrict")
 document_dt(BasicDistrict)
 document()
 
+##====telephone area code info ====
+
+data("BasicDistrict")
+data("BasicProvince")
+
+# get the province full code
+dt_code_pro <- BasicDistrict %>%
+  select(name, code) %>%
+  filter(name %in% BasicProvince$province) %>%
+  rename(district = "name",code_province = "code")
+
+# get area code and match province
+cols_list <- c("name", "code", "area_code")
+dt_district <- BasicDistrict %>%
+  select(all_of(cols_list)) %>%
+  filter(!is.na(area_code)) %>%
+  distinct(area_code, .keep_all = T) %>%
+  # string padding
+  mutate(code_province = str_pad(str_extract(as.character(code), "\\d{2}"), side = "right", width = 6, pad = "0")) %>%
+  left_join(., dt_code_pro, by = "code_province") %>%
+  filter(!is.na(district),
+         str_length(code)==6)
+
+# write out
+BasicTelcode <- dt_district
+usethis::use_data(BasicTelcode, overwrite = TRUE)
+
+# write document
+require(devtools)
+load_all()
+use_r("BasicTelcode")
+document_dt(BasicTelcode)
+document()
