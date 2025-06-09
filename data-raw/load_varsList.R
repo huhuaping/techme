81dc9bade5f5024dcb44beeec0eba0b3b3a950ef
huhuaping <huhuaping01@nwsuaf.edu.cn>
2025-06-09 17:05:54 +0800

    renew varsList pubobsStation variables order

--- data-raw/load_varsList.R
+++ data-raw/load_varsList.R
@@ -9,20 +9,17 @@ library("usethis")
 file_path <- "data-raw/varsList.xlsx"
 out_path <- "data-raw/varsOut.xlsx"
 varsList <- openxlsx::read.xlsx(file_path) %>%
-  mutate(variables= stringr::str_c(block1, block2, block3, block4, sep = "_"))
+  mutate(variables = stringr::str_c(block1, block2, block3, block4, sep = "_"))
 
-#write.xlsx(varsList, out_path)
+# write.xlsx(varsList, out_path)
 
 # check
 
 check <- varsList %>%
-  filter(block1 =="v8") %>%
+  filter(block1 == "v8") %>%
   select(chn_block2:chn_block4) %>%
   janitor::get_dupes()
 
-if (nrow(check) >0) stop( "some encoding rows duplicated!")
+if (nrow(check) > 0) stop("some encoding rows duplicated!")
 
 usethis::use_data(varsList, overwrite = TRUE)
-
-
-
