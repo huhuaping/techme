388979db0b0defb17850971faa114498d7032a0d
huhuaping <huhuaping01@nwsuaf.edu.cn>
2026-07-14 14:38:32 +0800

    2026 start

---
+++ .quarto/vdoc/.vdoc.7378eeab-2b96-4c60-8a97-a762ba0adf59.r
@@ -0,0 +1,33 @@
+#
+#
+#
+#
+#
+#
+#
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  fig.path = "man/figures/README-",
+  out.width = "100%"
+)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
