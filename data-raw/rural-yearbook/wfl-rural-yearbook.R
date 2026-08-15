17107bd52345cfa7c8d8e1ceb181c39f6fe4b46e
huhuaping <huhuaping01@nwsuaf.edu.cn>
2026-08-15 19:28:20 +0800

    renew dataset AgriPlastic

--- data-raw/rural-yearbook/wfl-rural-yearbook.R
+++ data-raw/rural-yearbook/wfl-rural-yearbook.R
@@ -20,7 +20,7 @@ pattern_sel <- choose.filePattern(
 find_result <- wfl.findFiles(
     dt = tbl_dir, # the directory table
     dir.case = "agri_prod", # the case name of the target directory
-    i.final = 2, # the index of the final subdirectory
+    i.final = 3, # the index of the final subdirectory
     pattern = pattern_sel # the regex pattern for table identifier
 )
 
@@ -131,7 +131,7 @@ df_vars_matched <- wfl.matchVars(
     block_lang = "eng" # default value is "eng"
 )
 
-#View(df_vars_matched)
+View(df_vars_matched)
 
 ## check the raw variables names in the varsList ----
 vars_spc # see before
